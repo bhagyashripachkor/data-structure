698477936e092c0e062bd15d3e37771dbb8c8b5d
Bhagyashri Pachkor <bhagyashrip547@gmail.com>
2016-05-06 23:19:35 +0530

    Added files via upload

---
+++ BinarySearchTree/Node.java
@@ -0,0 +1,13 @@
+
+public class Node<E> {
+ 
+    public E value;
+    public Node<E> leftChild;
+    public Node<E> rightChild;
+    
+    public Node(E value2) {
+    this.value = value2;
+    }
+    
+    
+}
